b982607c1cc033c84182a734b4c3b9ec273bcadb
ashtanov <aleksandr-shtanov@yandex.ru>
2016-05-05 18:50:50 +0300

    course init

---
+++ IndexApi/Service1.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace IndexApi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "IndexApi" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select IndexApi.svc or IndexApi.svc.cs at the Solution Explorer and start debugging.
+    public class IndexApi : IIndexApi
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
---
+++ SearchEngineTools/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngineTools
+{
+    public static class Extensions
+    {
+        public static bool ContainsSubSeq(this IEnumerable<string> text, IList<string> quote)
+        {
+            int i = 0;
+            foreach (var word in text)
+            {
+                if (i == quote.Count)
+                    return true;
+                if (word.Equals(quote[i]))
+                    i++;
+                else
+                    i = 0;
+            }
+            return false;
+        }
+    }
+}
