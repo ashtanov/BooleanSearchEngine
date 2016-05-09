e75a4e3f1f526ddf00ff28add48fd0fd6fb9d332
ashtanov <aleksandr-shtanov@yandex.ru>
2016-05-10 00:26:46 +0300

    Добавил лабу по нлп + извлекатель инфы из трекера

---
+++ NLPLabs/Ext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLPLabs
+{
+    static public class Ext
+    {
+        public static void TryAdd<K, V>(this Dictionary<K, V> d, K key, V value, Func<V, V, V> updateFunc)
+        {
+            V o;
+            if (d.TryGetValue(key, out o))
+                d[key] = updateFunc(o, value);
+            else
+                d.Add(key, value);
+        }
+    }
+}
